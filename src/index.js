720bfabf1bdbb398b14ed6f51355992c7ff95017
davidbzip <david@zippie.com>
2023-03-02 20:52:32 +0100

    simplifying app

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { GoogleOAuthProvider } from '@react-oauth/google';
+/*index.js*/
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <GoogleOAuthProvider clientId="312661605047-pdj38s2t7on3kvf8bi0mhol94otac9mg.apps.googleusercontent.com">
+            <App />
+    </GoogleOAuthProvider>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
