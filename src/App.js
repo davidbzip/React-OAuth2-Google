f3dc301689cca864fdd926b74490c26e65815b38
davidbzip <david@zippie.com>
2023-03-03 16:32:27 +0100

    modification related to get request for id token retrieval

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import { googleLogout, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
 
 function App() {
-    const [ user, setUser ] = useState([]);
-    const [ profile, setProfile ] = useState([]);
+
+    const [ profile, setProfile ] = useState({});
 
     const login = useGoogleLogin({
-        onSuccess: (codeResponse) => setUser(codeResponse),
+        onSuccess: async (tokenResponse) => {
+        await axios.get(
+          'https://www.googleapis.com/oauth2/v3/userinfo',
+          { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
+          ).then((res) => {
+            setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+        },
         onError: (error) => console.log('Login Failed:', error)
     });
-
-    useEffect(
-        () => {
-            if (user) {
-                axios
-                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
-                        headers: {
-                            Authorization: `Bearer ${user.access_token}`,
-                            Accept: 'application/json'
-                        }
-                    })
-                    .then((res) => {
-                        setProfile(res.data);
-                    })
-                    .catch((err) => console.log(err));
-            }
-        },
-        [ user ]
-    );
 
     // log out function to log the user out of google and set the profile array to null
     const logOut = () => {
         googleLogout();
-        setProfile(null);
+        setProfile({});
     };
 
     return (
@@ -42,12 +31,13 @@ function App() {
             <h2>React Google Login</h2>
             <br />
             <br />
-            {profile.name? (
+            {Object.keys(profile).length? (
                 <div>
                     <img src={profile.picture} alt="user" />
                     <h3>User Logged in</h3>
                     <p>Name: {profile.name}</p>
                     <p>Email Address: {profile.email}</p>
+                    <p>User ID: {profile.sub}</p>
                     <br />
                     <br />
                     <button onClick={logOut}>Log out</button>
